417d363fe20a3ad70a64d0eebb1bdad6ed796a99
AnnaArustamyan <128910436+AnnaArustamyan@users.noreply.github.com>
2024-07-21 20:39:28 +0400

    Add files via upload

---
+++ push_swap/includes/push_swap.h
@@ -0,0 +1,17 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdio.h>
+
+typedef struct s_list
+{
+    int value;
+    int index;
+    struct s_list *next;
+}                   t_list;
+
+#endif 
