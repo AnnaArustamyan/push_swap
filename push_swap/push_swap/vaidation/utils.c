887b1a15d5cd6fde315de49378dca28c749d7e51
AnnaArustamyan <128910436+AnnaArustamyan@users.noreply.github.com>
2024-07-21 22:55:28 +0400

    Add files via upload

---
+++ push_swap/push_swap/vaidation/utils.c
@@ -0,0 +1,43 @@
+#include "../includes/push_swap.h"
+
+int check_syntax(char **str)
+{
+    char    *syntax_str;
+
+    syntax_str = "0123456789+- ";
+
+    while (*str)
+    {
+        if (consists_of(*str, syntax_str) == 0)
+              return (0);
+        str++;
+    }
+    return (1);
+}
+
+int consists_of(char *str, char* syntax)
+{
+    int i;
+    int j;
+    int found;
+
+    i = 0;
+    while (str[i])
+    {
+        j = 0;
+        found = 0;
+        while (syntax[j])
+        {
+            if(str[i] == syntax[j])
+            {
+                found = 1;
+                break;
+            }
+            j++;
+        }
+        if(!found)
+            return (0);
+        i++;
+    }
+    return (1);
+}
